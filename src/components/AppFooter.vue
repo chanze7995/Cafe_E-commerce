9923bba0693fdd85463aec01e524e51bba58280b
Chanze Wu <chanze7995@gmail.com>
2022-03-30 10:08:42 +0800

    Create Header & Footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,28 @@
 <template>
-  <header>尾尾</header>
+  <footer>
+    <div class="newsLetterContainer">
+      <p>註冊我們的時事通訊以接收特別優惠、活動邀請等。</p>
+      <router-link to="/account">
+        加入我們
+      </router-link>
+    </div>
+    <div class="footerInfoContainer">
+      <div class="footerInfoContent">
+        <div class="footerInfoContent__title">
+          Caffeine with Mike or Coffee with Milk
+        </div>
+        <div class="footerInfoContent__text">
+          Calling:01-2345-6789
+        </div>
+        <div class="footerInfoContent__text">
+          caffeineWithMike@gmail.com
+        </div>
+      </div>
+      <div class="copyright">
+        ⓒ 2020-2022 Created by Chanze Wu .<br>僅個人練習使用，不做任何商業用途<br>Only for personal practice, no any commercial application.
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,63 @@
 <template>
-  <header>頭頭</header>
+  <header>
+    <ul :class="['headerNavbar', { mobileMenuOpen: isMobileMenuOpen }]">
+      <li class="headerNavbar__item">
+        <router-link to="/">
+          首頁
+        </router-link>
+      </li>
+      <li class="headerNavbar__item">
+        <router-link to="/explore">
+          探索咖啡
+        </router-link>
+      </li>
+      <li class="headerNavbar__item">
+        <router-link to="/shop">
+          線上購物
+        </router-link>
+      </li>
+      <li class="headerNavbar__logoImg">
+        <router-link to="/">
+          <img
+            src="@/assets/images/logo.svg"
+            alt="logo"
+          >
+        </router-link>
+      </li>
+      <li class="headerNavbar__item">
+        <router-link to="/location">
+          門市據點
+        </router-link>
+      </li>
+      <li class="headerNavbar__item">
+        <router-link to="/course">
+          咖啡課程
+        </router-link>
+      </li>
+      <li class="headerNavbar__item">
+        <router-link to="/login">
+          會員
+        </router-link>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+  name: 'AppHeader',
+  setup () {
+    const isMobileMenuOpen = ref(false)
+    const toggleMobileMenu = () => {
+      isMobileMenuOpen.value = !isMobileMenuOpen.value
+    }
 
+    return {
+      isMobileMenuOpen,
+      toggleMobileMenu
+    }
+  }
 }
 </script>
 
