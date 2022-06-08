d51df97297f2bcdff7f4630342cc8d81e7661feb
Chanze Wu <chanze7995@gmail.com>
2022-06-08 15:19:16 +0800

    Create Dropdown Menu Component

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="dropdownMenuContainer"
+    @click="toggleDropdownMenu"
+  >
+    <div class="dropdownMenu-btn">
+      <h4 class="dropdownMenu-title">
+        {{ filterTitle }}
+      </h4>
+
+      <SvgIcon
+        icon-name="chevron-down"
+        icon-class=""
+      />
+    </div>
+    <ul
+      class="dropdownMenu-option"
+      v-show="isDropdownMenuOpen"
+    >
+      <li
+        v-for="(item, i) in props.dropdownItems"
+        :key="i"
+        class="option-item"
+        @click="filterOption"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+export default {
+  props: {
+    dropdownTitle: {
+      type: String,
+      default: 'Click Me'
+    },
+    dropdownItems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup (props, { emit }) {
+    const dropdownTitle = ref(props.dropdownTitle)
+    const filterTitle = computed(() => {
+      console.log(dropdownTitle.value)
+      return dropdownTitle.value
+    })
+    const isDropdownMenuOpen = ref(false)
+    const toggleDropdownMenu = () => {
+      isDropdownMenuOpen.value = !isDropdownMenuOpen.value
+    }
+    console.log(props.dropdownItems)
+    const filterOption = (e) => {
+      dropdownTitle.value = e.target.innerText
+      emit('filter-items', dropdownTitle.value)
+    }
+    return {
+      props,
+      filterTitle,
+      isDropdownMenuOpen,
+      toggleDropdownMenu,
+      filterOption
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/_dropdownMenu.scss';
+</style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>{{ productData.length }}</div>
+  <div>
+    <section class="banner-container">
+      <div class="banner-img">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/caffeine-cafe.appspot.com/o/shop%2Fshop-banner.jpg?alt=media&token=5bea6dab-1332-4154-a8ef-74653c89b7a1"
+          alt=""
+        >
+      </div>
+      <div class="banner-text-container">
+        <h1>家中最優質的咖啡</h1>
+        <h4>合適的咖啡，讓你度過完美的一整天</h4>
+      </div>
+    </section>
+    <section class="product-list-container">
+      <div class="sort-btn">
+        <DropdownMenu
+          dropdown-title="選擇排序"
+          :dropdown-items="filterProductTitleData"
+          @filter-items="filterProduct"
+        />
+      </div>
+
+      <div>hi</div>
+    </section>
+    <div>{{ productData.length }}</div>
+  </div>
 </template>
 
 <script>
 // import ProductMenu from '@/components/shop/ProductMenu.vue'
 // import ProductExhibit from '@/components/shop/ProductExhibit.vue'
 // import ProductLightbox from '@/components/shop/ProductLightbox.vue'
-import { computed } from 'vue'
+import DropdownMenu from '@/components/DropdownMenu.vue'
+
+import { computed, reactive } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
   components: {
-
+    DropdownMenu
   },
   setup () {
     // 取得商品資料
@@ -19,8 +46,19 @@ export default {
     const productData = computed(() => {
       return store.getters.productData
     })
+    const filterProductTitleData = reactive([
+      '按價格排序：低到高',
+      '按價格排序：高到低',
+      '按折扣排序：高到低',
+      '按最新排序',
+      '按默認排序'])
+    const filterProduct = (str) => {
+      console.log('1', str)
+    }
     return {
-      productData
+      productData,
+      filterProductTitleData,
+      filterProduct
     }
   }
 }
