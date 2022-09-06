bc957dff291f457c0f6b1f4d861a8331234ac72e
Chanze Wu <chanze7995@gmail.com>
2022-09-06 21:09:31 +0800

    Set RWD of Login Component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,176 +1,181 @@
 <template>
-  <div :class="['registerContainer',{signUpMode:isFormSwitch}]">
-    <div class="formsContainer">
-      <div class="signin-signup">
-        <form
-          action="#"
-          class="signInForm"
-        >
-          <h2 class="formTitle">
-            會員登入
-          </h2>
-          <div class="formInputField">
-            <SvgIcon
-              icon-name="envelope"
-              icon-class="formInputField__icon"
-            />
-            <input
-              type="email"
-              placeholder="信箱"
-              v-model="singInData.email"
-            >
-          </div>
-          <div class="formInputField">
-            <SvgIcon
-              icon-name="lock"
-              icon-class="formInputField__icon"
-            />
-            <input
-              type="password"
-              placeholder="密碼"
-              autocomplete="off"
-              v-model="singInData.password"
-            >
-          </div>
-          <div
-            class="submitBtn"
-            @click="singInAccount"
+  <div>
+    <div class="header-background" />
+    <div :class="['register__container',{'sign-up--active':isFormSwitch}]">
+      <div class="forms__container">
+        <div class="forms">
+          <form
+            action="#"
+            class="sign-in__form"
           >
-            開始品嚐
-          </div>
-          <p class="socialText">
-            或用社群平台登入
-          </p>
-          <div class="socialMediaBtnContainer">
-            <a
-              v-for="item in socialMedia"
-              :href="item.link"
-              class="socialMediaBtn"
-              :key="item.type"
-            >
+            <h2 class="form__title">
+              會員登入
+            </h2>
+            <div class="form__input">
               <SvgIcon
-                :icon-name="item.type"
-                icon-class="socialMediaBtn__icon"
+                icon-name="envelope"
+                icon-class="symbol-icon"
               />
-            </a>
-          </div>
-        </form>
-        <form
-          action="#"
-          class="signUpForm"
-        >
-          <h2 class="formTitle">
-            註冊會員
-          </h2>
-          <div class="formInputField">
-            <SvgIcon
-              icon-name="user"
-              icon-class="formInputField__icon"
-            />
-            <input
-              type="text"
-              placeholder="使用者"
-              v-model="registerData.name"
+              <input
+                type="email"
+                placeholder="信箱"
+                v-model="singInData.email"
+              >
+            </div>
+            <div class="form__input">
+              <SvgIcon
+                icon-name="lock"
+                icon-class="symbol-icon"
+              />
+              <input
+                type="password"
+                placeholder="密碼"
+                autocomplete="off"
+                v-model="singInData.password"
+              >
+            </div>
+            <div
+              class="submit__btn"
+              @click="singInAccount"
             >
-          </div>
-          <div class="formInputField">
-            <SvgIcon
-              icon-name="envelope"
-              icon-class="formInputField__icon"
-            />
-            <input
-              type="email"
-              placeholder="信箱"
-              v-model="registerData.email"
-            >
-          </div>
-          <div class="formInputField">
-            <SvgIcon
-              icon-name="lock"
-              icon-class="formInputField__icon"
-            />
-            <input
-              type="password"
-              placeholder="密碼"
-              autocomplete="off"
-              v-model="registerData.password"
-            >
-          </div>
-          <div
-            class="submitBtn"
-            @click="registerAccount"
+              開始品嚐
+            </div>
+            <p class="social-media__text">
+              或用社群平台登入
+            </p>
+            <div class="social-media__btn__container">
+              <a
+                v-for="item in socialMedia"
+                :href="item.link"
+                class="social-media__btn"
+                :key="item.type"
+              >
+                <SvgIcon
+                  :icon-name="item.type"
+                  icon-class="symbol-icon"
+                />
+              </a>
+            </div>
+          </form>
+          <form
+            action="#"
+            class="sign-up__form"
           >
-            加入享受
-          </div>
-          <p class="socialText">
-            或用社群平台註冊
-          </p>
-          <div class="socialMediaBtnContainer">
-            <a
-              v-for="item in socialMedia"
-              :href="item.link"
-              class="socialMediaBtn"
-              :key="item.type"
-            >
+            <h2 class="form__title">
+              註冊會員
+            </h2>
+            <div class="form__input">
               <SvgIcon
-                :icon-name="item.type"
-                icon-class="socialMediaBtn__icon"
+                icon-name="user"
+                icon-class="symbol-icon"
               />
-            </a>
+              <input
+                type="text"
+                placeholder="使用者"
+                v-model="registerData.name"
+              >
+            </div>
+            <div class="form__input">
+              <SvgIcon
+                icon-name="envelope"
+                icon-class="symbol-icon"
+              />
+              <input
+                type="email"
+                placeholder="信箱"
+                v-model="registerData.email"
+              >
+            </div>
+            <div class="form__input">
+              <SvgIcon
+                icon-name="lock"
+                icon-class="symbol-icon"
+              />
+              <input
+                type="password"
+                placeholder="密碼"
+                autocomplete="off"
+                v-model="registerData.password"
+              >
+            </div>
+            <div
+              class="submit__btn"
+              @click="registerAccount"
+            >
+              加入享受
+            </div>
+            <p class="social-media__text">
+              或用社群平台註冊
+            </p>
+            <div class="social-media__btn__container">
+              <a
+                v-for="item in socialMedia"
+                :href="item.link"
+                class="social-media__btn"
+                :key="item.type"
+              >
+                <SvgIcon
+                  :icon-name="item.type"
+                  icon-class="symbol-icon"
+                />
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="panels__container">
+        <div class="panel sign-in__panel">
+          <div class="panel__content">
+            <h3 class="panel__title">
+              新朋友？
+            </h3>
+            <p class="panel__description">
+              創建帳戶即可享受優惠
+            </p>
+            <button
+              class="button"
+              @click="switchFormHandler"
+            >
+              加入我們
+            </button>
           </div>
-        </form>
-      </div>
-    </div>
-    <div class="panelsContainer">
-      <div class="panel signInPanel">
-        <div class="panel__content">
-          <h3>新朋友？</h3>
-          <p>
-            創建帳戶即可享受優惠
-          </p>
-          <Button
-            :btn-msg="signUpBtnMsg"
-            class="panel__btn"
-            @click="switchFormHandler"
-          />
+          <img
+            src="@/assets/images/login/readingSideDoddle.svg"
+            class="panel__img"
+            alt=""
+          >
         </div>
-        <img
-          src="@/assets/images/login/readingSideDoddle.svg"
-          class="panel__img"
-          alt=""
-        >
-      </div>
-      <div class="panel singUpPanel">
-        <div class="panel__content">
-          <h3>我們的一員？</h3>
-          <p>
-            趕緊來沖煮咖啡吧！
-          </p>
-          <Button
-            :btn-msg="signInBtnMsg"
-            class="panel__btn"
-            @click="switchFormHandler"
-          />
+        <div class="panel sign-up__panel">
+          <div class="panel__content">
+            <h3 class="panel__title">
+              我們的一員？
+            </h3>
+            <p class="panel__description">
+              趕緊來沖煮咖啡吧！
+            </p>
+            <button
+              class="button"
+              @click="switchFormHandler"
+            >
+              登入會員
+            </button>
+          </div>
+          <img
+            src="@/assets/images/login/chillingDoddle.svg"
+            class="panel__img"
+            alt=""
+          >
         </div>
-        <img
-          src="@/assets/images/login/chillingDoddle.svg"
-          class="panel__img"
-          alt=""
-        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Button from '@/components/Button.vue'
 import { ref, reactive } from 'vue'
 import { useStore } from 'vuex'
 export default {
   name: 'Login',
-  components: {
-    Button
-  },
   setup () {
     const store = useStore()
     const singInData = reactive({
